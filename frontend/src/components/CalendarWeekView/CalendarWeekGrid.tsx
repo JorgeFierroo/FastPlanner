3861ec5e2e5943c6ac6e19a8c0ebb1f9316d4864
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2025-09-21 20:05:42 -0300

    VistaCalendario: se pueden mover tareas de un dia a otro

--- frontend/src/components/CalendarWeekView/CalendarWeekGrid.tsx
+++ frontend/src/components/CalendarWeekView/CalendarWeekGrid.tsx
@@ -6,10 +6,11 @@ type CalendarGridProps = {
     week: number; // semana del mes (1-5)
     month: number;  // 0=enero, 1=febrero, ..., 11=diciembre
     year: number;
-    tasks: Record<string, { id: number; title: string; status: string }[]>; // Mapa de fechas a tareas
+    tasks: { id: number; title: string; status: string; date: string }[]; // Mapa de fechas a tareas
     daySelectFunction: (dateKey: string) => void; // Función para seleccionar un día
+    handleTaskDrop: (taskId: number, newDate: string) => void; // Función para manejar drop de tarea
 };
-export default function CalendarWeekGrid({ selectedDate, week, month, year, tasks, daySelectFunction }: CalendarGridProps) {
+export default function CalendarWeekGrid({ selectedDate, week, month, year, tasks, daySelectFunction, handleTaskDrop }: CalendarGridProps) {
 
     // obtener dia 1 de la semana
     const firstDayOfWeek = new Date(year, month, (week - 1) * 7 + 1).getDay() || 7; // Día de la semana del primer día de la semana (0=dom, 1=lun,...6=sáb), si es domingo poner 7
@@ -18,18 +19,18 @@ export default function CalendarWeekGrid({ selectedDate, week, month, year, task
     const days = []; // arreglo que contiene daycells con key = números, ya sea del mes o del mes anterior/siguiente
 
     function createDayCell(datekey: string, tasklist: { id: number; title: string; status: string }[]) {
-            const tasksForDate = tasks[datekey] || [];
+            const tasksForDate = tasks.filter(task => task.date === datekey);
             const today = `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, "0")}-${String(new Date().getDate()).padStart(2, "0")}`;
             return (
                 <DayCell
-                    key={datekey}
                     dayNumber={parseInt(datekey.split("-")[2])}
                     isToday={datekey === today}
                     isSelected={datekey === selectedDate}
                     clickFunction={() => daySelectFunction(datekey)}
                     tasks={tasksForDate}
                     mode="week"
-                />
+                    dateKey={datekey}
+                    onTaskDrop={handleTaskDrop} />
             );
         }
     // Agregar días del mes actual
@@ -44,16 +45,16 @@ export default function CalendarWeekGrid({ selectedDate, week, month, year, task
         if (dayOfMonth <= 0) {
             const daysInPrevMonth = new Date(year, month, 0).getDate(); // Último día del mes anterior
             const date = `${year}-${String(month).padStart(2, "0")}-${String(daysInPrevMonth + dayOfMonth).padStart(2, "0")}`;
-            days.push(createDayCell(date, tasks[date] || [])); // Días del mes anterior
+            days.push(createDayCell(date, tasks.filter(task => task.date === date))); // Días del mes anterior
         } else {
             if (dayOfMonth > daysInMonth) {
             // Si se excede el número de días del mes, agregar días del mes siguiente
             const date = `${year}-${String(month + 2).padStart(2, "0")}-${String(dayOfMonth - daysInMonth).padStart(2, "0")}`;
-            days.push(createDayCell(date, tasks[date] || [])); // Días del mes siguiente
+            days.push(createDayCell(date, tasks.filter(task => task.date === date))); // Días del mes siguiente
         } else {
             // Día del mes actual
             const date = `${year}-${String(month + 1).padStart(2, "0")}-${String(dayOfMonth).padStart(2, "0")}`;
-            days.push(createDayCell(date, tasks[date] || [])); // Días del mes actual:
+            days.push(createDayCell(date, tasks.filter(task => task.date === date))); // Días del mes actual:
         }}
 
     }
