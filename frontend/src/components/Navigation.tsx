3633f500bc7c541dd787178e94eb0fa6a134a08b
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2025-09-23 11:54:25 -0300

    Link hacia pagina de creación de tareas agregado

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -9,11 +9,12 @@ const Navigation: React.FC = () => {
     { path: "/projects", label: "Proyectos" },
     { path: "/profile", label: "Perfil" },
     { path: "/login", label: "Login" },
+    { path: "/tasks", label: "Tareas" },
     { path: "/Vistas", label: "Vistas"}
   ];
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+    <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">th
       <nav className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3">
         {/* Logo */}
         <div className="flex items-center space-x-2">
