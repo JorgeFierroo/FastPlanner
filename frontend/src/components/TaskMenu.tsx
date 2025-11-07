4b040754352c9f23f48b8f6839fc8b5b1dc696ea
Tomas <tpenroz2024@alu.uct.cl>
2025-11-07 00:05:51 -0300

    cambio de color de registro y de inicio de sesion, cambios menor en la pag de tareas

--- frontend/src/components/TaskMenu.tsx
+++ frontend/src/components/TaskMenu.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Edit2, Trash2, MoreVertical } from "lucide-react";
 
 type Props = {
     onDelete: ()=> void;
@@ -10,16 +11,17 @@ function TaskMenu({onDelete, onEdit}: Props){
 
     return(
         <div className="relative inline-block text-left">
-            <button onClick={()=> setIsOpen(!isOpen)} className="p-2 rounded hover:bg-gray-200">
-                 ⁝
+            <button onClick={()=> setIsOpen(!isOpen)} className="p-2 my-2 rounded-md hover:bg-gray-100 transition-all duration-200">
+                 <MoreVertical className="w-5 h-5 text-gray-600" />
             </button>
+
             {isOpen && (
-                <div className="absolute right-0 mt-2 w-32 bg-white border rounded shadow-lg z-10">
-                    <button onClick={()=> {onEdit(); setIsOpen(false);}} className="block w-full text-left px-4 py-2 hover:bg-gray-100">
-                        Editar
+                <div className="absolute right-0 mt-2 w-32 bg-white border border-gray-200 rounded-xl shadow-lg z-10">
+                    <button onClick={()=> {onEdit(); setIsOpen(false);}} className="flex items-center gap-2 w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 transition-all rounded-t-xl">
+                        <Edit2 className="w-4 h-4"/>Editar
                     </button>
-                    <button onClick={()=> {onDelete(); setIsOpen(false);}} className="block w-full text-left px-4 py-2 hover:bg-gray-100">
-                        Eliminar
+                    <button onClick={()=> {onDelete(); setIsOpen(false);}} className="flex items-center gap-2 w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 transition-all rounded-b-xl">
+                        <Trash2 className="w-4 h-4"/>Eliminar
                     </button>
                 </div>
             )}
