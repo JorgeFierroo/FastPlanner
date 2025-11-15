08330f536404bd07e14f80e1ffba31b721e0a706
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2025-11-15 17:58:34 -0300

    Cambios en la api (interceptor de accesstoken invalido) y pagina proyectos muestra datos correspondientes

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,4 +1,5 @@
 import Navigation from "./Navigation";
+import { TriangleAlert } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
 import React from "react";
 
@@ -18,9 +19,11 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
 
     if (!isAuthenticated) {
         return (
-            <div className="min-h-screen flex items-center justify-center">
-                <div className="text-center">
-                    <p className="text-gray-600">Acceso denegado. Por favor, inicie sesión.</p>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+                <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                    <TriangleAlert className="mx-auto mb-4 text-yellow-500" size={48} />
+                    <h2 className="text-2xl font-semibold mb-2">Acceso denegado</h2>
+                    <p className="text-gray-700 mb-4">No tienes permiso para acceder a esta página. Por favor, <a href="/auth" className="text-indigo-600 hover:underline">inicia sesión</a>.</p>
                 </div>
             </div>
         );
